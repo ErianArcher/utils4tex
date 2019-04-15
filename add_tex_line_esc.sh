9a796a452c8331481f41e22300c7ea0b7b16a0bd
Liangjp <jungg1996@gmail.com>
2019-04-15 10:32:01 +0800

    Auto complement the format of latex document

--- add_tex_line_esc.sh
+++ add_tex_line_esc.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 input_text_file=$1
-sed 's/$/&\\\\/g' $input_text_file > $input_text_file.tex
+echo "\\documentclass{article}\\begin{document}" > $input_text_file.tex
+sed 's/$/&\\\\/g' $input_text_file >> $input_text_file.tex
+echo "\\end{document}" >> $input_text_file.tex
